721e3719eaa98e325457c338189dcd419724909e
Elin Malikzade <malikze0@gfpc021.cs.man.ac.uk>
2012-02-23 18:24:32 +0000

    meh

--- HolidayManager.java
+++ HolidayManager.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.Calendar;
 
 public class HolidayManager
 {
@@ -6,23 +7,25 @@ public class HolidayManager
   //stores the hardlimit to the number of holidays a driver can take
   private static int HOLIDAY_LIMIT = 25;
   private static int driverid;
-  private static Date start;
-  private static Date end;
+  private static Calendar start;
+  private static Calendar end;
   private static int daysRequested = 0;
   
   
   public static boolean requestHoliday(int driver_id, Date start_date, Date end_date)
   {
     driverid = driver_id;
-    start = start_date;
-    end = end_date;
+    start.setTime(start_date);
+    end.setTime(end_date);
     if(hasEnoughHolidays())
     {
-      Date tempDate = start;
-      for(i = 0;i < daysRequested; i++)
+      Calendar tempDate = start;
+      for(int i = 0;i < daysRequested; i++)
       {
         if(dayIsClear(tempDate))
           return false;
+	//int buf_day = tempDate.getDay();
+ 	tempDate = start;
         tempDate.add(Calendar.DAY_OF_MONTH, 1);
       }
     }
@@ -36,29 +39,31 @@ public class HolidayManager
       //connect to the database and retried the specified drivers details.
     database.openBusDatabase();
       //get holidays taken.
-    int holidaysTaken = DriverInfo.getHolidaysTaken(driver_id);
+    int holidaysTaken = DriverInfo.getHolidaysTaken(driverid);
       //if the sum of taken and requested exceeds the limit fail
-    Date calDate = startDate;
-    while(calDate.before(endDate)
+    Calendar calDate = start;
+    while(calDate.before(end))
     {
+      //int buf_day = calDate.getDay();
+      calDate = start;
       calDate.add(Calendar.DAY_OF_MONTH, 1);
       daysRequested++;
     
     }
-    return (holidaysTaken + daysRequested) < HOLIDAY_LIMIT
+    return (holidaysTaken + daysRequested) < HOLIDAY_LIMIT;
   
   }//hasEnoughHolidays
    
    //this method checks to see if a requested day has in excess of 10 drivers
    //on holiday.
-  private static boolean dayIsClear(Date givenDay)
+  private static boolean dayIsClear(Calendar givenDay)
   {
-    int numberTaken;
+    int numberTaken=0;
     int driverIDs[] = DriverInfo.getDrivers();
     database.openBusDatabase();
-      for(i = 0; i < driverIDs.length(); i++)
+      for(int i = 0; i < driverIDs.length; i++)
       {
-       if(!DriverInfo.isAvailable(i, givenDay))
+       if(!DriverInfo.isAvailable(i, givenDay.getTime()))
          numberTaken++;
       }
     return numberTaken > 10;
