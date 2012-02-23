e5df3956d69f015c0c628d8298200269abc53f5e
Paul Nunnerley <nunnerp0@gfpc018.cs.man.ac.uk>
2012-02-23 15:49:23 +0000

    Added day is clear method

--- HolidayManager.java
+++ HolidayManager.java
@@ -6,22 +6,29 @@ public class HolidayManager{
    
    
    public static void requestHoliday(int driver_id, Date start, Date end){
+     
    
    }
-   //when given a driver id this method check if the driver has enough holidays
-   //for their request to be allowed.
-   private static boolean hasEnoughHolidays(int driver_id, int daysRequested){
+
+   //method determines in the driver has enough available holidays for their request
+   private static boolean hasEnoughHolidays(int driver_id, Date startDate, Date endDate)
+   {
       //connect to the database and retried the specified drivers details.
       database.openBusDatabase();
       
       //get holidays taken.
       int holidaysTaken = DriverInfo.getHolidaysTaken(driver_id);
       
-      if(holidaysTaken + daysRequested > HOLIDAY_LIMIT)
-         return false;
-      else
-         return true;
+      //if the sum of taken and requested exceeds the limit fail
+      return (holidaysTaken + daysRequested) < HOLIDAY_LIMIT
+        
 
       
    }//hasEnoughHolidays
+   
+   //this method checks to see if a requested day has in excess of 10 drivers on holiday.
+   
+   private static boolean dayIsClear(Date day){
+   
+   }
 }
