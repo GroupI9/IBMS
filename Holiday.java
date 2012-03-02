fa506bdfbb39aa711e063f834e9d7f08ff41a253
Elin Malikzade <malikze0@gfpc041.cs.man.ac.uk>
2012-03-02 09:08:23 +0000

    some debugging done

--- Holiday.java
+++ Holiday.java
@@ -7,6 +7,7 @@ public class Holiday{
     private final int HOLIDAY_LIMIT = 25;
     //store the id for the driver requesting the holiday
     int driverId;
+    public int numberTaken;
    
     //instanciate the object
     public Holiday(int p_driverId, Calendar p_startDate, Calendar p_endDate){
@@ -14,48 +15,54 @@ public class Holiday{
         this.driverId = p_driverId;
         this.startDate = p_startDate;
         this.endDate = p_endDate;
-	Calendar acceptDates = startDate;
+	Calendar acceptDates = (Calendar) startDate.clone();
         if(hasEnoughDays() && allDaysAreClear()){
-
-            System.out.println("This holiday can be set.");
-            for(int i = 1; i < length(); i++){
-                //for each day set the driver unavilable
+            System.out.println("This holiday can be set.");            
+            for(int i = 1; i < length(); i++){                
+                //for each day set the driver unavilable                
                 DriverInfo.setAvailable(driverId, acceptDates.getTime(), true);
+                if(DriverInfo.isAvailable(driverId, acceptDates.getTime()))
+		  System.out.println("Set!!!");
                 acceptDates.add(Calendar.DAY_OF_MONTH, 1);
             }
+	    System.out.println("Done!!!"); 
+            if(dayIsClear(startDate))
+              System.out.println(numberTaken);           
         }
+        //database.close();
     }
     
     //returns the length in days of this holiday request
-    public int length(){
+    public int length(){               
         return (int)( (endDate.getTime().getTime() - startDate.getTime().getTime())/(1000 * 60 * 60 * 24));
     }
 
-    private boolean hasEnoughDays(){
-        int currentCount = DriverInfo.getHolidaysTaken(driverId);
+    private boolean hasEnoughDays(){        
+        int currentCount = DriverInfo.getHolidaysTaken(driverId);        
         return (currentCount + this.length()) < HOLIDAY_LIMIT;
     }
+    
     private boolean allDaysAreClear(){
-      Calendar tempDate = startDate;
+      Calendar tempDate = (Calendar) startDate.clone();
       for(int i = 0;i < length(); i++)
       {
-        if(dayIsClear(tempDate))
+        if(!dayIsClear(tempDate))
           return false;
         tempDate.add(Calendar.DAY_OF_MONTH, 1);
       }
       return true;
     }
+    
     private boolean dayIsClear(Calendar givenDay)  {
-    int numberTaken=0;
-    int driverIDs[] = DriverInfo.getDrivers();
-    database.openBusDatabase();
+      numberTaken=0;
+      int driverIDs[] = DriverInfo.getDrivers();
+      database.openBusDatabase();
       for(int i = 0; i < driverIDs.length; i++)
       {
-       if(!DriverInfo.isAvailable(i, givenDay.getTime()))
-         numberTaken++;
+        if(!DriverInfo.isAvailable(driverIDs[i], givenDay.getTime()))
+          numberTaken++;
       }
-    return numberTaken > 10;
-  
-  }
-
+      System.out.println(numberTaken);
+      return numberTaken < 10;  
+    }
 }
