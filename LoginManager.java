d064dcb67e01436c2702d14d69880bbb3223365c
Antonios Saliba <salibaa0@gfpc040.cs.man.ac.uk>
2012-03-16 09:15:23 +0000

    Antonio

---
+++ LoginManager.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class LoginManager
+{
+  private final int driverID;
+  public LoginManager(int givenDriverId)
+  {
+    driverID=givenDriverId; 
+    if(idValid())
+  } 
+  public boolean idValid()
+  { 
+    //int check = DriverInfo.findDriver(driverID);
+    //for(int i = 0;i < 70;
+  } 
+}
