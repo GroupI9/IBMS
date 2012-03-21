c12f15f41360e1f6a9efb7b15cbb6108a3f0c88b
Miren Pau <paum0@gfpc046.cs.man.ac.uk>
2012-03-21 16:14:26 +0000

    miren

---
+++ service.java
@@ -0,0 +1,13 @@
+public class service
+{
+  public int serviceid;
+  public int starttime;
+  public int endtime;
+  
+  public service(int id, int start, int end) 
+  {
+    serviceid = id;
+    starttime = start;
+    endtime = end;
+  }
+}
