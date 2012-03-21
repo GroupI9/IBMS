8e00405f8d3994611d1017e29dba72d49bf37bde
Elin Malikzade <malikze0@gfpc047.cs.man.ac.uk>
2012-03-21 19:13:32 +0000

    g

---
+++ driverUsed.java
@@ -0,0 +1,13 @@
+public class driverUsed
+{
+  public int driver_id;
+  public int startTime;
+  public int endTime;
+
+  public driverUsed(int id, int start, int end)
+  {
+    driver_id = id;
+    startTime = start;
+    endTime = end;
+  }
+}
