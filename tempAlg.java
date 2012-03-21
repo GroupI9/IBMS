0947af60fe3d3d5195626d2d6a4e3ebdf15d0364
Miren Pau <paum0@gfpc046.cs.man.ac.uk>
2012-03-21 16:13:36 +0000

    serviceclass

--- tempAlg.java
+++ tempAlg.java
@@ -11,6 +11,9 @@ public class tempAlg
   private int serviceIDs[];
   private int busToRoute[][];
   private int todayservicetimes[];
+  private int numberofservices = 0;
+  private service dailyservices[];
+  private int servicearrayindex = 0;
   
   public tempAlg(Calendar sDate)
   {
@@ -23,35 +26,50 @@ public class tempAlg
     busIDs = BusInfo.getBuses();//get all buses
     
     Date thisday;//convert cal to date obj
-    //TimetableInfo today = new TimetableInfo();//get timetablekind
-
     Calendar tempCal = (Calendar)startDate.clone();
-    for(int i = 0; i < 7; i++)//for each day
-    {
+   // for(int i = 0; i < 7; i++)//for each day
+   // {
       thisday = tempCal.getTime();//convert cal to date obj
-      //today.todaytime = TimetableInfo.timetableKind(thisday);//get timetablekind
-      System.out.println(tempCal.getTime());
+      for(int j = 0; j < routeIDs.length; j++)
+      {
+        numberofservices += TimetableInfo.getNumberOfServices(routeIDs[j], 
+					TimetableInfo.timetableKind(thisday));
+      }
+      dailyservices = new service[numberofservices];
       for(int j = 0; j < routeIDs.length; j++)//for each route
       {
-	System.out.println("Route: " + routeIDs[j]);
-	serviceIDs = TimetableInfo.getServices(routeIDs[j], TimetableInfo.timetableKind(thisday));	
-	for(int k = 0; k < serviceIDs.length; k++)//for each service
+	serviceIDs = TimetableInfo.getServices(routeIDs[j],
+					 TimetableInfo.timetableKind(thisday));
+	todayservicetimes = TimetableInfo.getTimingPoints(routeIDs[j]);
+	int num = TimetableInfo.getNumberOfServices(routeIDs[j],
+					TimetableInfo.timetableKind(thisday));
+	numberofservices += serviceIDs.length;
+	for(int k = 0; k < num; k++)//for each service
 	{
-	  System.out.println("Service: " + serviceIDs[k]);
-	  todayservicetimes = TimetableInfo.getServiceTimes(routeIDs[j], TimetableInfo.timetableKind(thisday), serviceIDs[k]);
-	  toString(todayservicetimes);  
+	  todayservicetimes = TimetableInfo.getServiceTimes(routeIDs[j],
+	 				 TimetableInfo.timetableKind(thisday),k);
+	  service thisservice = new service(serviceIDs[k], todayservicetimes[0], 
+	  				todayservicetimes[todayservicetimes.length
+					- 1]);
+	  dailyservices[servicearrayindex] = thisservice;
+	  servicearrayindex++;
 	}
       }
       tempCal.add(Calendar.DATE, 1);
-    }
-    //  todayservicetimes = TimetableInfo.getServiceTimes(routeIDs[1], today.todaytime,
-   //   serviceIDs[1]);
-   //   toString(todayservicetimes);    
+      numberofservices = 0;
+      toString(dailyservices);
+    //}
   }
   
-  public static void toString(int[]array)
+  public static void toString(service[]array)
   {
     for(int i = 0; i < array.length; i++)
-      System.out.println(array[i]);
+      System.out.println(array[i].serviceid + " " + array[i].starttime + " " +
+      array[i].endtime);
+  }
+  
+  public static void sortServices(int[]array)
+  {
+ 
   }  
 }
