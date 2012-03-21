557f50d92e79c20630ee01d0edf00fa2cdaa888f
Elin Malikzade <malikze0@gfpc047.cs.man.ac.uk>
2012-03-21 19:13:40 +0000

    g

--- RosterManager.java
+++ RosterManager.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Pack
 {
-  int service_id;
+  service serv;
   int bus_id;
   int driver_id;
 }
@@ -24,38 +24,62 @@ public class RosterManager
   private service dailyservices[];
   private int todayservicetimes[];
   private int servicearrayindex = 0;
+  private driverUsed[] driversUsed;
+  private int driversUsedIndex = 0;
+  private busUsed[] busesUsed;
+  private int busesUsedIndex = 0;
+  private service[] todayServices;
   
-  public RosterManager(Calendar startDate, Calendar endDate)
+  public RosterManager(Calendar startDay)
   {
-    this.startDate = startDate;
-    this.endDate = endDate;
-    numberOfDays = (endDate.getTime().getTime() - startDate.getTime().getTime())/(1000 * 60 * 60 * 24); 
+    database.openBusDatabase();
+    startDate = (Calendar) startDay.clone();
+    int getday = 0;
+    if(startDate.get(Calendar.DAY_OF_WEEK) != 2)
+    {
+      getday = startDate.get(Calendar.DAY_OF_WEEK);
+      if(getday == 1)
+        getday = 6;
+      else
+	getday = (getday - 2); 
+      startDate.add(Calendar.DATE, -getday);
+    }  
+    
+    endDate = (Calendar)startDate.clone();
+    endDate.add(Calendar.DATE, 6);   
+    
+    numberOfDays = 7; 
     packs = new Pack[numberOfDays][];   
     buses = BusInfo.getBuses();
     drivers = DriverInfo.getDrivers();
     minutesWorked = new int[drivers.length];
     numberOfServices = 0;
-    routes = BusStopInfo.getRoutes();
+    routes = BusStopInfo.getRoutes();   
+    driversUsed = new driverUsed[drivers.length]; 
+    busesUsed = new busUsed[buses.length]; 
+
   }
 
 
   public void createRoster()
   {
     Calendar today = (Calendar) startDate.clone();
-    int[] todayServices;
+    int count;   
     for(int i=0; i<numberOfDays; i++)
     {
-      int count = 0;      
-      numberOfServices = getNumberOfServices(today); 
+      count = 0;      
+      numberOfServices = getNumberOfServices(today.getTime()); 
       todayServices = getServices(today);				// correct it!
       packs[i] = new Pack[numberOfServices];
       for(int j=0; j<numberOfServices; i++)
       {
-        pack[i][j].service_id = getService()	;			// correct it!
-	pack[i][j].driver_id = getLeastWorkingDriver(drivers, today);   // correct it!
-        pack[i][j].bus_id = getAvailableBus();				// correct it!
-        setDriverUnavailable(pack[i][j].driver_id);
-	setBusUnavailable(pack[i][j].bus_id);        
+        System.out.println("" + i + " " + j);
+        packs[i][j].serv = getNextService();		// correct it!
+	packs[i][j].driver_id = getLeastWorkingDriver(drivers, today.getTime(), packs[i][j].serv);   // correct it!
+        packs[i][j].bus_id = getAvailableBus(buses, today.getTime(), packs[i][j].serv);				// correct it!
+//        setDriverUnavailable(packs[i][j].driver_id);
+//	setBusUnavailable(packs[i][j].bus_id);        
+        minutesWorked[packs[i][j].driver_id] += (packs[i][j].serv.endtime) - (packs[i][j].serv.starttime);
       }	    
       today.add(Calendar.DAY_OF_MONTH, 1);
     }    
@@ -64,70 +88,116 @@ public class RosterManager
 
   private int getNumberOfServices(Date day)
   {
-    int numberOfServicesToday;
+    int numberOfServicesToday = 0;
     for(int j=0; j<routes.length; j++)
-      numberOfServicesToday = TimetableInfo.getNumberOfServices(route, day); // have to be corrected for 358
+      numberOfServicesToday += TimetableInfo.getNumberOfServices(routes[j], day); // have to be corrected for 358
     return numberOfServicesToday;
   }
+
+
 
   private service[] getServices(Calendar day)
   {    
     dailyservices = new service[numberOfServices];
     for(int j = 0; j < routes.length; j++)//for each route
     {
-      services = TimetableInfo.getServices(routeIDs[j],
-					 TimetableInfo.timetableKind(day));
-      int num = TimetableInfo.getNumberOfServices(routeIDs[j],
-					TimetableInfo.timetableKind(thisday));
+      services = TimetableInfo.getServices(routes[j],
+					 TimetableInfo.timetableKind(day.getTime()));
+      int num = TimetableInfo.getNumberOfServices(routes[j],
+					TimetableInfo.timetableKind(day.getTime()));
       for(int k = 0; k < num; k++)//for each service
       {
-        todayservicetimes = TimetableInfo.getServiceTimes(routeIDs[j],
-	 				 TimetableInfo.timetableKind(thisday),k);
-	service thisservice = new service(serviceIDs[k], todayservicetimes[0], 
+        todayservicetimes = TimetableInfo.getServiceTimes(routes[j],
+	 				 TimetableInfo.timetableKind(day.getTime()),k);
+	service thisservice = new service(services[k], todayservicetimes[0], 
 	  				todayservicetimes[todayservicetimes.length
 					- 1]);
 	dailyservices[servicearrayindex] = thisservice;
         servicearrayindex++;
       }
     }
-    for(int i = 0; i < array.length; i++)
-      System.out.println(array[i].serviceid + " " + array[i].starttime + " " 
-     					 + array[i].endtime);   
+    return dailyservices;   
   }
 
 
-  private int[] concat(int[] A, int[] B) 
+  private service getNextService()
   {
-    int[] C= new int[A.length+B.length];
-    System.arraycopy(A, 0, C, 0, A.length);
-    System.arraycopy(B, 0, C, A.length, B.length);
-    return C;
+    int startTime = 1500;
+    int index=0;
+    for(int i=0; i<todayServices.length; i++)
+    {
+      if(todayServices[i].starttime < startTime && !todayServices[i].used)
+      {
+        index = i;
+	startTime = todayServices[i].starttime;
+      }
+    }
+    todayServices[index].used = true;
+
+    return todayServices[index];
   }
 
-
-  private int[] getService()
-  {
-  }
-
-  private int getLeastWorkingDriver(int[] driversList, Calendar day)
+  private int getLeastWorkingDriver(int[] driversList, Date day, service serv)
   {
     int minMins = 3000;
     int driver_id=0;
+    boolean temp = false;
     for(int i=0; i<driversList.length; i++)
     {
-      if(minutesWorked[i] < minMins && DriverInfo.isAvailable(driverList[i], day))
-      {
-        driver_id = i;
-        minMins = minutesWorked[i];
+      if(minutesWorked[i] < minMins && DriverInfo.isAvailable(driversList[i], day))
+      {        
+        for(int j=0; j<driversUsedIndex; j++)
+	{	  
+          if(driversList[i] == driversUsed[j].driver_id)
+	  {
+            if(serv.starttime > driversUsed[j].endTime)
+	      temp = true;
+	    else
+	      temp = false;	    
+	  }  	  
+	}   
+	if(temp)
+	{
+	  driver_id = driversList[i];
+          minMins = minutesWorked[i];
+	  driversUsed[driversUsedIndex] = new driverUsed(driver_id, serv.starttime, serv.endtime);
+	  driversUsedIndex++;
+	}    
       }      
-    }
+    }  
+      
     return driver_id;
   }
 
-  private int getAvailableBus()
+  private int getAvailableBus(int[] busesList, Date day, service serv)
   {
+    int bus_id = 0;
+    boolean temp = false;
+    for(int i=0; i<buses.length; i++)
+    {
+      if(BusInfo.isAvailable(busesList[i], day))
+      {
+        for(int j=0; j<busesUsedIndex; j++)
+        {
+	  if(busesList[i] == busesUsed[j].bus_id)
+	  {
+	    if(serv.starttime > busesUsed[j].endTime)
+	      temp = true;
+	    else
+	      temp = false;
+	  }
+        }
+	if(temp)
+	{
+	  bus_id = busesList[i];
+	  busesUsed[busesUsedIndex] = new busUsed(bus_id, serv.starttime, serv.endtime);
+	  busesUsedIndex++;
+	}
+      }
+    }
+    return bus_id;
   }
-
+/*
   private void setDriverUnavailable(int driver_id)
   {
   }
@@ -142,5 +212,6 @@ public class RosterManager
 
   private void setBusAvailable(int bus_id)
   {
-  }   
+  }  
+*/ 
 }
