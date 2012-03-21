10bc6737e68d288ad2739c825311fe0ad98fc172
Elin Malikzade <malikze0@gfpc047.cs.man.ac.uk>
2012-03-21 19:13:24 +0000

    g

---
+++ busUsed.java
@@ -0,0 +1,13 @@
+public class busUsed
+{
+  public int bus_id;
+  public int startTime;
+  public int endTime;
+
+  public busUsed(int id, int start, int end)
+  {
+    bus_id = id;
+    startTime = start;
+    endTime = end;
+  }
+}
