d22d394de8c68e8bdedbc0e1f517bfdbb95e0bac
Miren Pau <paum0@gfpc079.cs.man.ac.uk>
2012-03-21 13:20:40 +0000

    miren

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 import java.util.Date;
-
+import java.util.Calendar;
 
 /*
  * A very simple application illustrating how to use the interface.
@@ -15,11 +15,11 @@ public class Main {
      */
     public static void main(String[] args) {
        
-        LoginGui login = new LoginGui();
-	login.setVisible(true);
-        database.openBusDatabase();
-        int[] driverIDs = DriverInfo.getDrivers();
-        String[] driverNames = new String [driverIDs.length];
+      // LoginGui login = new LoginGui();
+    //   login.setVisible(true);
+   //    database.openBusDatabase();
+   //    int[] driverIDs = DriverInfo.getDrivers();
+   //    String[] driverNames = new String [driverIDs.length];
         //for (int i=0; i<driverIDs.length; i++)
             //System.out.println(DriverInfo.getName(driverIDs[i]));
 
@@ -34,7 +34,26 @@ public class Main {
         boolean test = HolidayManager.requestHoliday(driverIDs[0], s, e);  
         if(test)
          System.out.println("Wohooo!");*/
-        
+	
+	/*get a date and turn it into the monday of the given date*/
+	
+	Calendar startDate = Calendar.getInstance();
+	startDate.set(2012, 0, 25);
+	Calendar endDate = Calendar.getInstance();
+	endDate.set(2012, 5, 21);
+        int getday = 0;
+	if(startDate.get(Calendar.DAY_OF_WEEK) != 2)
+	{
+	  getday = startDate.get(Calendar.DAY_OF_WEEK);
+	  if(getday == 1)
+	    getday = 6;
+	  else
+	    getday = (getday - 2);
+	  
+	  startDate.add(Calendar.DATE, -getday);
+	}    
+	
+	tempAlg testalg = new tempAlg(startDate);
     }
 
 }
